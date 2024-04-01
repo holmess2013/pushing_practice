0736ac80c23535feeb6eddbffd541ac6bfd912dc
Sam Holmes <samuel.holmes@uga.edu>
2024-04-01 15:48:37 -0400

    changed scalar_multiplication.py
    I made a new branch where I have changed scalar_multiplication.py to carry out matrix multiplication instead. Now the script is called matrix_multiplication.py

---
+++ matrix_multiplication.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+matrix_1 = np.array([[1,2],[3,4]])
+matrix_2 = np.array([[5,6],[7,8]])
+
+product = np.dot(matrix_1, matrix_2)
+
+
+print(product)
--- scalar_multiplication.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-num_array = np.array([1,2,3,4])
-
-scaled_num_array = num_array*2
-
-print(scaled_num_array)
